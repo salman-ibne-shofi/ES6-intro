9ba80552c49c6e0eb911e917a11d859fb844115a
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-03-02 15:11:11 +0600

    Template string multiple line string dynamic tring

---
+++ template.js
@@ -0,0 +1,28 @@
+const first = 'jaan'
+const last = 'pakhi'
+const greet = 'potas potas';
+const name = first + ' ' + last + " " + greet;
+
+console.log(name);
+const a = 10;
+const b =20;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+
+const numbers = [45, 48, 98, 78]
+const student = {name: 'john ochena', age: 5}
+// const math = `The sum of ${numbers[0]} and ${b} is ${a + b}`
+const math = `The sum of ${numbers[0]} and ${student.age} is ${numbers[0] + student.age}`
+console.log(math)
+
+const email = 'Hi john \n' + 
+'cena cena lage \n' +
+'tumi mara mari korba naki'
+;
+console.log(email)
+
+const challenge = `john cena
+cena naki ochena
+mair dimu prochur`;
+
+console.log(challenge)
